e201c95788ea481283976e306ca3180b32a968da
cjj <3054833457@qq.com>
2017-12-27 13:35:17 +0800

    check code bugs

--- supernotes/views.py
+++ supernotes/views.py
@@ -16,18 +16,21 @@ def index(request):
 
 
 def sign_in(request, *args, **kwargs):
-    password = request.POST.get('password')
-    username = request.POST.get('username')
-    ids = request.POST['ids']
-    ids = request.GET['ids']
-    ids = json.loads(ids)
-    return HttpResponse(json.dumps(ids))
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            return HttpResponseRedirect('/')
-    return render(request, 'login.html')
+    # 点击登录请求模态框
+    if request.method.lower() == 'get':
+        return render(request,'login.html')
+    # 点击登录，判断用户名密码是否正确模块
+    elif request.method.lower() == 'post':
+        password = request.POST.get('password')
+        username = request.POST.get('username')
+        user = authenticate(username=username, password=password)
+        # 登录成功，从数据库匹配用户名密码
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        # 匹配失败，用户名或者密码错误
+        return render(request, 'login.html')
 
 
 def sign_out(request):
